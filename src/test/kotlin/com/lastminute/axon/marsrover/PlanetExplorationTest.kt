20b4d76fdb79d9c7745d9d42c542d007c8fc7640
cerealk <granaglia@gmail.com>
2021-11-01 16:50:29 +0100

    now we have planetary cartography

--- src/test/kotlin/com/lastminute/axon/marsrover/PlanetExplorationTest.kt
+++ src/test/kotlin/com/lastminute/axon/marsrover/PlanetExplorationTest.kt
@@ -2,43 +2,52 @@ package com.lastminute.axon.marsrover
 
 import com.lastminute.axon.marsrover.domain.command.PlanetMap
 import com.lastminute.axon.marsrover.domain.command.Position
-import com.lastminute.axon.marsrover.domain.command.Rover
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.eventsourcing.EventSourcingHandler
-import org.axonframework.eventsourcing.GenericAggregateFactory
 import org.axonframework.modelling.command.AggregateIdentifier
-import org.axonframework.spring.eventsourcing.SpringPrototypeAggregateFactory
+import org.axonframework.modelling.command.AggregateLifecycle
 import org.axonframework.spring.stereotype.Aggregate
 import org.axonframework.test.aggregate.AggregateTestFixture
+import org.axonframework.test.aggregate.FixtureConfiguration
 import org.junit.jupiter.api.Test
 
 class PlanetExplorationTest {
 
-    private val  fixture: AggregateTestFixture<Planet> = AggregateTestFixture(Planet::class.java)
+    private val fixture: FixtureConfiguration<PlanetCartography> = AggregateTestFixture(PlanetCartography::class.java)
+
 
     @Test
     internal fun `can probe the surface of a planet`() {
-        fixture.`when`(ProbePlanetCommand("Mars"))
+        fixture
+            .registerInjectableResource(TestSatelliteProbeService(listOf(Position(3, 7))))
+            .given()
+            .`when`(ProbePlanetCommand("Mars"))
             .expectSuccessfulHandlerExecution()
-            .expectEvents(PlanetMappedEvent("Mars", PlanetMap(listOf(Position(3,7)))))
+            .expectEvents(PlanetMappedEvent("Mars", PlanetMap(listOf(Position(3, 7)))))
     }
 }
 
 @Aggregate
-class Planet {
+class PlanetCartography {
 
     @AggregateIdentifier
-    val planetName:String
+    private lateinit var planetName: String
 
     @CommandHandler
-    constructor(command: ProbePlanetCommand){
-        planetName = command.planetName
+    constructor(command: ProbePlanetCommand, externalService: TestSatelliteProbeService) {
+        AggregateLifecycle.apply(PlanetMappedEvent(command.planetName, PlanetMap(externalService.probe())))
+    }
+
+    @EventSourcingHandler
+    fun on(event: PlanetMappedEvent) {
+        planetName = event.planetName
     }
 }
 
+data class ProbePlanetCommand(val planetName: String)
+data class PlanetMappedEvent(val planetName: String, val planetMap: PlanetMap)
 
-//class PlanetAggregateFactory(): SpringPrototypeAggregateFactory<Planet>(){}
 
-
-data class ProbePlanetCommand(val planetName:String)
-data class PlanetMappedEvent(val planetName:String, val planetMap:PlanetMap)
+class TestSatelliteProbeService(private val obstaclesCoordinates: List<Position>) {
+    fun probe() = obstaclesCoordinates
+}
