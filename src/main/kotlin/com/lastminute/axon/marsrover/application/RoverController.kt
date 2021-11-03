e1844a26cb476a9493d99255986c33ecf62d6b35
cerealk <granaglia@gmail.com>
2021-11-03 18:44:12 +0100

    improved api

--- src/main/kotlin/com/lastminute/axon/marsrover/application/RoverController.kt
+++ src/main/kotlin/com/lastminute/axon/marsrover/application/RoverController.kt
@@ -1,6 +1,7 @@
 package com.lastminute.axon.marsrover.application
 
 import com.lastminute.axon.marsrover.domain.command.Orientation
+import com.lastminute.axon.marsrover.domain.command.Orientation.N
 import com.lastminute.axon.marsrover.domain.command.Position
 import com.lastminute.axon.marsrover.domain.coreapi.DropLanderCommand
 import com.lastminute.axon.marsrover.domain.coreapi.FollowPathCommand
@@ -10,9 +11,7 @@ import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
 import org.axonframework.queryhandling.QueryGateway
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 //TODO: improve api :D
@@ -27,18 +26,19 @@ class RoverController {
 
     val commandParser = CommandParser()
 
-    @RequestMapping("rover/{moves}")
-    fun move(@PathVariable("moves") moves: String): Trail {
-        val commands = commandParser.parseCommands(moves)
-
-        //a command creating an aggregate returns the Id of the aggreegate itself (?)
-        val res = commandGateway.sendAndWait<Any>(
-            DropLanderCommand("Mars1", Position(1,2), Orientation.N)
-        )
-
-        //TODO: here the return value is null
-        commandGateway.sendAndWait<Any>(FollowPathCommand("Mars1", commands))
-
-        return queryGateway.query(TrailQuery("Mars1"), Trail::class.java).get()
+    @PostMapping("/{planetName}/{roverName}")
+    fun drop(@PathVariable("planetName") planet: String, @PathVariable("roverName")rover: String){
+        commandGateway.send<Any>(DropLanderCommand(planet, Position(1,1), N))
     }
+
+
+    @PostMapping("/{planetName}/{roverName}/{moves}")
+    fun move(@PathVariable("roverName") rover: String, @PathVariable("moves") moves: String){
+
+        val commands = commandParser.parseCommands(moves)
+        commandGateway.send<Any>(FollowPathCommand(rover, commands))
+    }
+
+    @GetMapping("/{planetName}/{roverName}/trail")
+    fun trailFor(@PathVariable("roverName") rover: String) = queryGateway.query(TrailQuery(rover), Trail::class.java)
 }
